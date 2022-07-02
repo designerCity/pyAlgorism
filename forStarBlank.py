35314e51911c5bf15698b3c565ae02d8e3cf5fcd
designerCity <97098829+designerCity@users.noreply.github.com>
2022-07-02 21:46:50 +0900

    Update forStarBlank.py

--- forStarBlank.py
+++ forStarBlank.py
@@ -5,3 +5,9 @@ for i in range(1, (star+1)):
     star_num = i * '*'
     blank_num = " " * (star - i)
     print(blank_num + star_num)
+
+    
+# 더 간단한 풀이
+star=int(input())
+for i in range(1,(star+1)):
+    print(" "*(star-i) + "*"*i)
