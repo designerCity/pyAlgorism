94480aa086a0000c29d096fdd902228535cd8968
designerCity <97098829+designerCity@users.noreply.github.com>
2022-07-07 19:01:24 +0900

    Create 4344.py

---
+++ 4344.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+for i in range(n):
+    scores = list(map(int, input().split()))
+    
+    avg = sum(scores[1:]) / scores[0]
+    person = 0
+    for j in scores[1:]:
+        if j > avg:
+            person += 1
+    
+    rate = person / scores[0] * 100
+    # percent = round((person / scores[0] * 100), 3)
+    print(f'{rate:.3f}%')  
