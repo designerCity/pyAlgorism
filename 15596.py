717fe6350438a222735a8a2afde5f26c4acb6562
designerCity <97098829+designerCity@users.noreply.github.com>
2022-07-07 19:22:16 +0900

    Create 15596.py

---
+++ 15596.py
@@ -0,0 +1,15 @@
+def solve(a):
+    ans = 0
+    for i in a:
+        ans += i
+        
+    return ans
+
+# def solve(a):
+#     ans = 0
+#     for i in range(len(a)):
+#         ans+=a[i]
+#     return ans
+
+# def solve(a):
+#     return sum(a)
