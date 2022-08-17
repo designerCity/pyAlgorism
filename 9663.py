156cc88a98799584f978324dc1a33c6b6c6828d0
designerCity <97098829+designerCity@users.noreply.github.com>
2022-08-18 02:56:31 +0900

    Update 9663.py

--- 9663.py
+++ 9663.py
@@ -1,42 +1,33 @@
-# N - Queen
-
-# N - Queen 문제는 크기가 N * N 인 체스판 위에 퀸 N 개를 서로 공격할 수 없게 놓는 문제이다. 
-# N 이 주어졌을 때, 퀸을 놓는 방법의 수를 구하는 프로그램을 작성해라
-
 # Queen 은 같은 index 를 가질 수도, 같은 줄에 있을 수도 없다.
 # # 대각선도 안됨.
 n = int(input())
-# 체스판
-# # list 안에 list 를 집어 넣어야 한다.
+# 체스판 한 줄
 chess = []
 for i in range(n):
     chess.append(True)
-
+# # 체스판
 all = []
 for j in range(n):
     all.append(chess)
 
-
-
 cnt = 0
 # # 경우의 수를 탐색
-def dfs(idx): # idx 는 각 줄을 의미
+def dfs(depth): # idx 는 각 줄을 의미
     global all, cnt
-    if idx == n:
+    if depth == n :
         cnt += 1
         return
-    # Queen이 각 줄에서 올 수 있는 경우의 수
-    for i in range(len(all[idx])):
-        if all[idx][i] == True:
-            dfs(idx + 1)
-            
-    # # 안되는 것을 지우는 코드
-    # for i in range(len(all[idx])):
-    #     all[idx][idx] = True
-    #     if idx + i <= len(idx) - 1:
-    #         all[idx][idx + i] = True
-    #     if idx - i >= 0:
-    #         all[idx][idx - i]
 
+    # Queen이 각 줄에서 올 수 있는 경우의 수
+    for i in range(n): # i 는 row 의 인덱스 을 의미한다.
+        if all[depth][i] == True:     # depth 줄의 i 번째 index 가 기준이 된다.
+            for j in range(depth, n): # j 지울려고 하는 row
+                all[j][i] = False     # 같은 세로 줄
+                if i + j - depth < n:
+                    all[j][i + j - depth] = False  # 대각선 오른쪽
+                if i - j + depth >= 0:
+                    all[j][i - j + depth]  = False # 대각선 오른쪽
+            dfs(depth + 1)
+            
 dfs(0)
 print(cnt)
