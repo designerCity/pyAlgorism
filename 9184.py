68f1623b3866acacb26dceaa9e476dbccfb7bbb4
designerCity <97098829+designerCity@users.noreply.github.com>
2022-09-06 18:32:02 +0900

    Add files via upload

--- 9184.py
+++ 9184.py
@@ -1,23 +1,28 @@
+import sys
+input = sys.stdin.readline
 
-# print('w(%i), {%i}, {%i}) = {}' % (a,  b,  c) )
 def w(a, b, c):
     if a <= 0 or b <= 0 or c <=0:
         return 1
-    if a > 20 or b > 20 or c > 20:
+
+    if a > 20 or b > 20 or c > 20: # a, b, c 중에서 20 보다 큰 수가 존재하면, 모두 20으로
         return w(20, 20, 20)
 
-
+    if dp[a][b][c]:
+        return dp[a][b][c]
     if a < b and b < c:
-        return w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
-    else:
-        return w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+        dp[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+        return dp[a][b][c]
+    dp[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+    return dp[a][b][c]
 
+dp = [[[0] * (21) for _ in range(21)] for _ in range(21)]
 
 while 1:
     a, b, c = map(int, input().split())
-    
     if a== -1 and b == -1 and c == -1:
         break
-    
+
     d = w(a, b, c)
     print('w(%i, %i, %i) = %i' % (int(a),  int(b),  int(c), int(d)))
+    # print(f'w({a}, {b}, {c}) = {w(a,b,c)}') fString
