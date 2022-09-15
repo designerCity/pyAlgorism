45293d8472155d12845fa5a1c52097dcdba1ec43
designerCity <97098829+designerCity@users.noreply.github.com>
2022-09-15 10:57:59 +0900

    Add files via upload

--- 9461.py
+++ 9461.py
@@ -5,12 +5,13 @@ t = int(input())
 
 for _ in range(t):
     case = int(input())
-    n = case
+    n = 5
     # 나선에서 가장 긴 변의 길이를 k 라고 한다.
     p_li = [1, 1, 1, 2, 2, 3]
-    while n > 
-    if n > 5:
-        p_li.append(p_li[case] + p_li[case - 4])
-    else:
+    while case > n: 
+        
+        p_li.append(p_li[n] + p_li[n - 4])
+        n +=1
+            
 
     print(p_li[case-1]) # 출력
