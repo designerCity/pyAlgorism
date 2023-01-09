d1eabe56b7926c4efe0b7649faabcbd65cf0ece4
designerCity <97098829+designerCity@users.noreply.github.com>
2023-01-10 00:11:00 +0900

    Update 1463.py

--- 1463.py
+++ 1463.py
@@ -1,1 +1,21 @@
 
+n = int(input())
+cnt = 0
+
+while n != 1:
+    if n % 3 == 1:
+        n-=1
+        cnt += 1
+        if n % 3 == 0:
+            n = n  // 3
+            cnt += 1
+        elif n % 2 == 0:
+            n = n // 2
+            cnt += 1
+    elif n % 3 == 0:
+        n = n  // 3
+        cnt += 1
+    elif n % 2 == 0:
+        n = n // 2
+        cnt += 1
+print(cnt)
